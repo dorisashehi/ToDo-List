6bac0f50c3551f6e3f5a01d823cc9ddd54698f48
Dorisa Shehi <doris.shehi2@gmail.com>
2024-04-28 16:01:16 -0500

    Empty task and dialog added

--- src/index.js
+++ src/index.js
@@ -7,3 +7,15 @@ document.querySelector(".toggle").addEventListener("click", () => {
     document.querySelector(".sidebar-col").classList.toggle('hide');
 
 })
+
+const dialog = document.getElementById("dialog");
+
+document.querySelector(".fa-plus-circle").addEventListener("click", () => {
+    dialog.showModal();
+})
+
+const closeDialog = dialog.querySelector("#js-close");
+closeDialog.addEventListener("click", (e) => { //close dialog box
+    e.preventDefault();
+    dialog.close();
+});
